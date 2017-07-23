26c04ed412beee5d1b17a216f0fcd5e27a332dcd
LaPetiteSouris <sontunghoang@gmail.com>
2017-07-23 11:51:14 +0200

    Cralw with focused

--- crawl.py
+++ crawl.py
@@ -1,5 +1,6 @@
 import subprocess
-from urllib2 import urlopen, Request
+from urllib2 import Request, urlopen
+
 import scrapy
 from bs4 import BeautifulSoup
 from scrapy.linkextractors import LinkExtractor
@@ -7,22 +8,27 @@ from scrapy.linkextractors import LinkExtractor
 
 class MangaSpider(scrapy.Spider):
     name = "manga"
+    manga_name = "touch-tam-voi"
     start_urls = ["http://uptruyen.com/manga/147313/comedy/touch-tam-voi.html"]
+    black_list = []
 
     def start_requests(self):
         for url in self.start_urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def shouldDownload(self, url):
+        if url in self.black_list or self.manga_name not in url:
+            return False
         req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
         html = urlopen(req).read()
         soup = BeautifulSoup(html)
         sum_img = sum([1 for i in soup.find_all(
             'img')])
-        return sum_img > 50
+        return sum_img > 100
 
     def download(self, url):
-        bashCommand = "wget -k -p {url}".format(url=url)
+        bashCommand = "mkdir {manga_name}&&wget -k -p {url}".format(
+            manga_name=self.manga_name, url=url)
         process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
         output, error = process.communicate()
 
@@ -33,5 +39,6 @@ class MangaSpider(scrapy.Spider):
         for link in links:
             url_to = link.url
             if self.shouldDownload(url_to):
+                print url_to
                 # Download
-                self.download(url_to)
+                # self.download(url_to)
